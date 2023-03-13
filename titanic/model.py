21e8ecbc0191ffc8a3d3e4d94a65b1ddc9d36ad2
Tsvika S <tsvikas@dell>
2023-03-13 04:30:05 +0200

    notebook for model

--- titanic/model.py
+++ titanic/model.py
@@ -1,3 +1,19 @@
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_filter: -all
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.14.5
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
 # # titanic model
 
 # %load_ext autoreload
